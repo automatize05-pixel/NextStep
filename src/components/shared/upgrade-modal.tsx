cc07576580e3b0d81a6970e3155170d3610d6223
automatize05-pixel <automatize05@gmail.com>
2026-04-04 16:34:50 +0100

    fix: duplicate SVG attributes causing build failure

--- src/components/shared/upgrade-modal.tsx
+++ src/components/shared/upgrade-modal.tsx
@@ -120,9 +120,9 @@ function FileTextIcon(props: any) {
     >
       <path d="M14.5 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7.5L14.5 2z" />
       <polyline points="14 2 14 8 20 8" />
-      <line x1="16" y2="13" x1="8" y1="13" />
-      <line x1="16" y2="17" x1="8" y1="17" />
-      <line x1="10" y2="9" x1="8" y1="9" />
+      <line x1="8" y1="13" x2="16" y2="13" />
+      <line x1="8" y1="17" x2="16" y2="17" />
+      <line x1="8" y1="9" x2="10" y2="9" />
     </svg>
   )
 }
